19f8207b8399dab5ad3ceef62ef0d9ecfd9a0abc
maxkex <mkebets@gmail.com>
2024-06-12 16:54:10 -0700

    speed

--- src/main/java/csci12/KebetsProgram01.java
+++ src/main/java/csci12/KebetsProgram01.java
@@ -1,6 +1,7 @@
 package csci12;
 import Code12.Code12;
 import Code12.Code12Program;
+import Code12.GameObj;
 public class KebetsProgram01 extends Code12Program
 {
    // The main entry point for code12 runner in Visual Studio Code
@@ -11,8 +12,13 @@ public class KebetsProgram01 extends Code12Program
    // Start the program
    public void start()
    {
-      ct.text( "Hello from Code12!", 40, 10, 4, "red");
-      ct.circle( 10, 10, 20, "blue");
-      ct.rect(20, 20, 20, 10, "green");
+      ct.text( "Roll !!!", 40, 10, 4, "red");
+      GameObj ball = ct.circle( 10, 10, 20, "blue");
+      ball.setYSpeed( .1);
+      GameObj block = ct.rect(20, 20, 20, 10, "green");
+      block.setText("Block");
+      block.setXSpeed(.1);
+      int xBoundary = 100;
+      int yBoundary = 100;
    }
 }   // End of class
