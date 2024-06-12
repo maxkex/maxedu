4d0081539bb2ef845f030439f6460b30aa40620b
maxkex <mkebets@gmail.com>
2024-06-11 20:27:21 -0700

    Code12 hello works!

--- src/main/java/csci12/maxApp.java
+++ src/main/java/csci12/maxApp.java
@@ -1,28 +1,17 @@
 package csci12;
 import Code12.Code12;
 import Code12.Code12Program;
-import Code12.GameImage;
-public class maxMain extends Code12Program
+public class maxApp extends Code12Program
 {
-   // Declare fields here
-   GameImage img;
-   
    // The main entry point
    public static void main(String[] args)
    {
-      Code12.run(new maxMain()); 
+      Code12.run(new maxApp()); 
    }
-   
    // Start the program
+   @Override
    public void start()
    {
       ct.text( "Hello from Code12!", 50, 50, 15 );
-   }
-   
-   // Update the display
-   public void update()
-   {
-      // Draw the image
-      //img.draw();
    }
 }   // End of class
