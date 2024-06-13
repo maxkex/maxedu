efa808d6bb8b04f593d5e413ec8282fff481d624
maxkex <mkebets@gmail.com>
2024-06-13 14:26:04 -0700

    WIP

--- src/main/java/csci12/KebetsProgram01.java
+++ src/main/java/csci12/KebetsProgram01.java
@@ -12,13 +12,21 @@ public class KebetsProgram01 extends Code12Program
    // Start the program
    public void start()
    {
-      ct.text( "Roll !!!", 40, 10, 4, "red");
-      GameObj ball = ct.circle( 10, 10, 20, "blue");
+      Integer xMin = 2; // Left boundary of the screen
+      Integer xMax = 300; // Right boundary of the screen
+      Integer yMin = 2; // Top boundary of the screen
+      Integer yMax = 300; // Bottom boundary of the screen
+      // Set up the screen with text
+      ct.text( "Bounce!", 20, yMin, 4, "red");
+      ct.line(xMin, yMin+6, xMax, yMin+7,"orange");
+      // Create a ball and a block shapes and set their speed
+      Integer ballD = 20; // Height/Diameter of the ball
+      GameObj ball = ct.circle( xMin+ballD, yMin+ballD, ballD, "blue");
       ball.setYSpeed( .1);
-      GameObj block = ct.rect(20, 20, 20, 10, "green");
+      Integer blockH = 10;
+      Integer blockW = 20;
+      GameObj block = ct.rect( xMin+blockW, yMin+blockH, blockW, blockH, "green");
       block.setText("Block");
       block.setXSpeed(.1);
-      int xBoundary = 100;
-      int yBoundary = 100;
    }
 }   // End of class
